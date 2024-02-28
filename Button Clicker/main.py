dba8cd1b3f1ef06bdb1296622a90bad6d184db83
Jacob Wood <129340044+JacobWood12@users.noreply.github.com>
2024-02-28 13:22:08 +1300

    Made button clicker randomly say if you're safe or not.

--- Button Clicker/main.py
+++ Button Clicker/main.py
@@ -1,4 +1,7 @@
+# TKinter is for the GUI.
 from tkinter import *
+# Random is for the safe/unsafe message.
+import random
 clicks = int(0)
 
 def btn_click():
@@ -7,7 +10,15 @@ def btn_click():
 	if clicks == 1:
 		print("Button clicked once. You're safe.")
 	else:
-		print(f"Button clicked {clicks} times. You're safe.")
+		coin_flip = random.randint(1, 2)
+		if coin_flip == 1:
+			print(f"Button clicked {clicks} times. You're safe.")
+		elif coin_flip == 2:
+			print(f"Button clicked {clicks} times. You're in danger.")
+		else:
+			print("There was an error. Please try again.")
+			print("TECHNICAL DETAILS.")
+			print("The random number generator generated a number that was not 1 or 2.")
 
 window = Tk()
 btn = Button(window, text="Click me or else.", command=btn_click)
