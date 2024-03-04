0ba14b6b56f5e19476b9c12c6ad0380e7ca15110
JacobWood12 <21108@students.mrgs.school.nz>
2024-03-05 09:50:04 +1300

    Megre background into main. (#2)
    * Created background
    
    * Fixing folders
    
    * Fixing folders again.
    
    * Oops I deleted the buttons but I'm putting them back
    
    * Added a comment.

---
+++ GUI/Background/main.py
@@ -0,0 +1,25 @@
+# TKinter is for the GUI.
+from tkinter import *
+
+def red_click():
+    print("You pressed the red button.")
+    # Changes background to red.
+    window['bg']='#e45858'
+
+def blue_click():
+    print("You pressed the blue button.")
+    # Changes background to blue.
+    window['bg']='#6246ea'
+
+window = Tk()
+# Sets size of the window otherwise it's to small to see the background.
+window.geometry("400x200")
+
+# Create buttons.
+red_btn = Button(window, text="Red.", command=red_click)
+blue_btn= Button(window, text="Blue.", command=blue_click)
+
+red_btn.pack()
+blue_btn.pack()
+
+window.mainloop()
