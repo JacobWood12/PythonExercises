bb04592f273685a4d80f71cb41e8678375a8e978
JacobWood12 <525JacobW@gmail.com>
2024-03-06 12:09:43 +1300

    Added comments.

--- pet.py
+++ pet.py
@@ -1,10 +1,14 @@
 class Pet():
+    # Starts a name field and sets it to empty until the user gives it a name later on.
     name = None
+    # How hungry a pet is, zero being completely hungry and 100 being completely full.
+    # Shown to the user as a percentage.
     fullness = 0
 
     def __init__(self,name):
         self.name = name
 
+    # Takes a pet instance and a number representing a certain food as input.
     def eat(self, food):
         print(f"{self.name} is eating {food}...")
         if food == "1": # Carrots.
@@ -13,7 +17,7 @@ class Pet():
             self.fullness = self.fullness + 5
         elif food == "3": # Water.
             self.fullness = self.fullness + 1
-        else:
+        else: # I might add more foods in future.
             print("That's not a food.")
         print(f"{self.name} is now {self.fullness}% full.")
 
@@ -26,6 +30,7 @@ name = input("What would you like to call your pet? \n")
 # Creates an instance of the Pet class and sets it's name to what the user said earlier.
 pet_1 = Pet(name)
 
+# While the pet is below 100% full.
 while pet_1.fullness < 100:
     print(f"\n{pet_1.name} is hungry.")
     print(f"It's currently {pet_1.fullness}% full.")
