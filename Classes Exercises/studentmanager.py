4b3992065bc18e49600a7eaf7a03a4b3846721fc
JacobWood12 <525JacobW@gmail.com>
2024-03-06 14:37:33 +1300

    Create studentmanager.py

---
+++ Classes Exercises/studentmanager.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name):
+        self.first_name = name
+    
+    def display_name(self):
+        print(self.first_name)
+
+    def set_grade(self, grade):
+        self.grade = grade
+    
+    def display_grade(self):
+        print(self.grade)
+
+
+csc_level_2 = []
