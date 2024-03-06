788a000f0c76bdd73e7a11f6c761fc0e83733a05
JacobWood12 <525JacobW@gmail.com>
2024-03-06 14:51:41 +1300

    Added a student and Tkinter.

--- Classes Exercises/studentmanager.py
+++ Classes Exercises/studentmanager.py
@@ -1,3 +1,6 @@
+# Imports tkinter for GUI.
+from tkinter import *
+
 class Student:
     def __init__(self, name):
         self.first_name = name
@@ -8,8 +11,27 @@ class Student:
     def set_grade(self, grade):
         self.grade = grade
     
-    def display_grade(self):
-        print(self.grade)
+    def get_grade(self):
+        return self.grade
+    
+def show_grade():
+    # Show grade using a label.
+    grade_label.config(text=csc_l2[0].get_grade())
+    pass
 
 
-csc_level_2 = []
+csc_l2 = []
+
+csc_l2.append(Student("Jacob"))
+csc_l2[0].set_grade("Excellence.")
+
+window = Tk()
+window.geometry("300x300")
+
+grade_label = Label()
+grade_label.pack()
+
+show_grade_btn = Button(text="Show Grade.", command=show_grade)
+show_grade_btn.pack()
+
+window.mainloop()
