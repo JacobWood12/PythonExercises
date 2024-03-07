9b8762233a6fc20c70d6d0135e311ba490262c1b
JacobWood12 <525JacobW@gmail.com>
2024-03-08 12:11:28 +1300

    Started listbox.

--- Classes Exercises/studentmanager.py
+++ Classes Exercises/studentmanager.py
@@ -24,17 +24,25 @@ def show_grade():
 csc_2 = []
 
 # Adds me as a student with Excellence.
-csc_2.append(Student("Jacob"))
+csc_2.append(Student("Jacob."))
 csc_2[0].set_grade("Excellence.")
 
 # Adds Ken as a student with a grade of Achieved (he asked for it).
-csc_2.append(Student("Ken"))
+csc_2.append(Student("Ken."))
 csc_2[1].set_grade("Achieved.")
 
 # Sets up window.
 window = Tk()
 window.geometry("300x300")
 
+# Sets up listbox.
+students_listbox = Listbox(window)
+students_listbox.pack()
+
+students_listbox.insert(0, "Jacob.")
+students_listbox.insert(1, "Ken.")
+
+# Sets up grade label.
 grade_label = Label()
 grade_label.pack()
 
