fc3062ba149ebe2d6f00cf11023e95aa2f40de4e
JacobWood12 <21108@students.mrgs.school.nz>
2024-03-11 00:02:41 +1300

    Merge Studentmanager (#4) into main.
    * Create studentmanager.py
    
    * Added a student and Tkinter.
    
    * Added a student and started listbox.
    
    * Reorganised Student class.
    
    * Changed "csc_l2" to "csc_2" to match.
    
    * Added show grade button.
    
    * Oops I spelt "pack" with 2 Cs.
    
    * Started listbox.
    
    * Create quiz.py

---
+++ Classes Exercises/studentmanager.py
@@ -0,0 +1,53 @@
+# Imports tkinter for GUI.
+from tkinter import *
+
+# Starts the student class.
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_grade(self):
+        return self.grade
+
+    def set_grade(self, grade):
+        self.grade = grade
+
+    def display_name(self):
+        print(self.first_name)
+    
+def show_grade():
+    # Show grade using a label.
+    grade_label.config(text=csc_2[0].get_grade())
+
+# Starts the dictionary with currently no students.
+csc_2 = []
+
+# Adds me as a student with Excellence.
+csc_2.append(Student("Jacob."))
+csc_2[0].set_grade("Excellence.")
+
+# Adds Ken as a student with a grade of Achieved (he asked for it).
+csc_2.append(Student("Ken."))
+csc_2[1].set_grade("Achieved.")
+
+# Sets up window.
+window = Tk()
+window.geometry("300x300")
+
+# Sets up listbox.
+students_listbox = Listbox(window)
+students_listbox.pack()
+
+students_listbox.insert(0, "Jacob.")
+students_listbox.insert(1, "Ken.")
+
+# Sets up grade label.
+grade_label = Label()
+grade_label.pack()
+
+# Show grade button.
+show_grade_btn = Button(text="Show Grade.", command=show_grade)
+show_grade_btn.pack()
+
+window.mainloop()
