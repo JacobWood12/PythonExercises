08154b28a4cf6f3863778d6f119c1ae796f1d7b3
JacobWood12 <525JacobW@gmail.com>
2024-03-11 14:36:34 +1300

    Added question answers.

--- GUI/Quiz/quiz.py
+++ GUI/Quiz/quiz.py
@@ -10,6 +10,20 @@ global questions_answers
 asked = []
 score = int(0)
 
+questions_answers = {
+    1: ["What must you do when you see blue and red flashing lights behind you?", 'Speed up to get out of the way', 'Slow down and drive carefully', 'Slow down and stop', 'Drive on as usual', 'Slow down and stop',3],
+    2: ["You may stop on a motorway only:", 'if there is an emergency', 'To let down or pick up passengers', 'to make a U-turn', 'to stop and take a photo', 'if there is an emergency', 11],
+    3: ["When coming up to a pedestrian crossing without a raised traffic island, what must you do?", "Speed up before the pedestrians cross", 'Stop and give way to pedestrians on any part of the crossing', "Sound the horn on your vehicle to warn the pedestrians", "slow down to 30kmh", 'Stop and give way to pedestrians on any part of the crossing', 2],
+    4: ["Can you stop on a bus stop in a private motor vehicle?", 'Only between midnight and 6am', "Under no circumstances", "When dropping off passengers", 'Only if it is less than 5 minutes', "Under no circumstances", 2],
+    5: ["What is the maximum speed you may drive if you have a 'space saver wheel' fitted? (km/h)", '70 km/h', "100 km/h so you do not hold up traffic", "80 km/h and if the wheel spacer displays a lower limit that applies", "90 km/h", "80 km/h and if the wheel spacer displays a lower limit that applies",3],
+    6: ["When following another vehicle on a dusty road, you should:", 'Speed up to get passed',"Turn your vehicle's windscreen wipers on", "Stay back from the dust cloud", 'Turn your vehicles headlights on', "Stay back from the dust cloud", 31],
+    7: ["What does the sign containing the letters 'LSZ' mean", 'Low safety zone', "Low stability zone", "Lone star zone", 'Limited speed zone', 'Limited speed zone',4],
+    8: ["What speed are you allowed to pass a school bus that has stopped to let students get on or off?", '20 km/h', "30 km/h", "70 km/h", '10 km/h', '20 km/h',1], 
+    9: ["What is the maximum distance a load may extend in front of a car?", '2 meters forward of the front edge of the front seat', "4 meters forward of the front edge of the front seat", "3 meters forward of the front edge of the front seat", '2.5 meters forward of the front edge of the front seat', '3 meters forward of the front edge of the front seat',3],
+    10: ["To avoid being blinded by the headlights of another vehicle coming towards you what should you do?", 'Look to the left of the road', "Look to the centre of the road",
+"Wear sunglasses that have sufficient strength", 'Look to the right side of the road', 'Look to the left of the road',1],
+    }
+
 class QuizStarter:
     def __init__(self, parent):
         # Sets window background colour to a dark red.
