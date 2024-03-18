1e135ed3e6c56ada50a7ad626b18ea7de02e27ee
JacobWood12 <525JacobW@gmail.com>
2024-03-18 14:09:04 +1300

    Renamed "clicks" to "CLICKS".

--- GUI/button_clicker.py
+++ GUI/button_clicker.py
@@ -4,20 +4,20 @@ from tkinter import Tk, Button
 """ Random is for the safe/unsafe message. """
 import random
 
-clicks = int(0)
+CLICKS = int(0)
 
 def btn_click():
-    """ Runs every time the user clicks the button. """
-    global clicks
-    clicks = int(clicks + 1)
-    if clicks == 1:
+    """ Runs every time the user CLICKS the button. """
+    global CLICKS
+    CLICKS = int(CLICKS + 1)
+    if CLICKS == 1:
         print("Button clicked once. You're safe.")
     else:
         coin_flip = random.randint(1, 2)
         if coin_flip == 1:
-            print(f"Button clicked {clicks} times. You're safe.")
+            print(f"Button clicked {CLICKS} times. You're safe.")
         elif coin_flip == 2:
-            print(f"Button clicked {clicks} times. You're in danger.")
+            print(f"Button clicked {CLICKS} times. You're in danger.")
         else:
             print("There was an error. Please try again.")
             print("TECHNICAL DETAILS.")
