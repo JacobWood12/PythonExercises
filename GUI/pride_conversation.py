c1158bea9aa4fc1e64324ce1f3cd642c3e2d0399
JacobWood12 <525JacobW@gmail.com>
2024-05-23 20:52:34 +1200

    Copied pride_conversation.py from second section of video - Creating and navigating to screens.

--- GUI/pride_conversation.py
+++ GUI/pride_conversation.py
@@ -18,22 +18,54 @@ word_list = [
 	},
 ]
 
+word_to_display = random.choice(word_list)
+
 window = tk.Tk()
 window.geometry("300x600")
 window.title("Pride Conversation")
 
-word_to_display = random.choice(word_list)
+# Functions
 
-question_label = tk.Label(wrap=200, text="Do you know the words used in the rainbow community?")
+def show_meaning_screen():
+	WORD_screen.pack_forget()
+	MEANING_screen.pack()
+
+def next_word():
+	global word_to_display
+
+	word_to_display = random.choice(word_to_display)
+	MEANING_screen.pack_forget()
+	word_label.configure(text=word_to_display["word"])
+	meaning_label.configure(text=word_to_display["meaning"])
+	WORD_screen.pack()
+
+# WORD Screen - Displays the word and the buttons for 'I know this!' and 'Not sure!'
+WORD_screen = tk.Frame(window)
+WORD_screen.pack()
+
+
+question_label = tk.Label(WORD_screen, wrap=200, text="Do you know the words used in the rainbow community?")
 question_label.pack()
 
-word_label = tk.Label(text=word_to_display["word"])
+word_label = tk.Label(WORD_screen, text=word_to_display["word"])
 word_label.pack()
 
-yes_button = tk.Button(text="I know this!")
+yes_button = tk.Button(WORD_screen, text="I know this!", command=show_meaning_screen)
 yes_button.pack()
 
-no_button = tk.Button(text="Not sure.")
+no_button = tk.Button(WORD_screen, text="Not sure.", command=show_meaning_screen)
 no_button.pack()
+
+# MEANING screen - Displays the meaning of the words and the buttons 'I got this!' and 'I understood it differently'
+MEANING_screen = tk.Frame(window)
+
+meaning_label = tk.Label(MEANING_screen, text=word_to_display["meaning"])
+meaning_label.pack()
+
+correct_button = tk.Button(MEANING_screen, text="I got this!", command=next_word)
+correct_button.pack()
+
+wrong_button = tk.Button(MEANING_screen, text="I understood it differently", command=next_word)
+wrong_button.pack()
 
 window.mainloop()
