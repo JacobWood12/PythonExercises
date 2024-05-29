b4ad67ea234a937257561b8783acdee413edb8c2
JacobWood12 <525JacobW@gmail.com>
2024-05-29 14:38:10 +1200

    Renamed "btn" to "button".
    Readability!!!

--- GUI/compass.py
+++ GUI/compass.py
@@ -6,20 +6,20 @@ window.geometry("300x300")
 window.title("Compass")
 
 # North button.
-north_btn = Button(window, text="North.", command=(print("You pressed the north button.")))
-north_btn.pack(side="top")
+north_button = Button(window, text="North.", command=(print("You pressed the north button.")))
+north_button.pack(side="top")
 
 # South button.
-south_btn = Button(window, text="South.", command=(print("You pressed the south button.")))
-south_btn.pack(side="bottom")
+south_button = Button(window, text="South.", command=(print("You pressed the south button.")))
+south_button.pack(side="bottom")
 
 # East button.
-east_btn = Button(window, text="East.", command=(print("You pressed the east button.")))
-east_btn.pack(side="right")
+east_button = Button(window, text="East.", command=(print("You pressed the east button.")))
+east_button.pack(side="right")
 
 # West button.
-west_btn = Button(window, text="West.", command=(print("You pressed the west button.")))
-west_btn.pack(side="left")
+west_button = Button(window, text="West.", command=(print("You pressed the west button.")))
+west_button.pack(side="left")
 
 # North label.
 north_label = Label(window, text="North.")
